ee7b538b459b5bdcf57d22d1f1ee5a6802f88210
Wahab896 <memonwahab19@gmail.com>
2024-06-20 15:53:07 +0530

    Refactored code with multi values input handling

--- src/add.js
+++ src/add.js
@@ -1,7 +1,8 @@
 function add(inputs) {
     if (!inputs) return 0;
+    let numValues = inputs.split(',');
     let output;
-    return output;
+    return numValues.reduce((acc, i) => acc += parseInt(i), 0);
 }
 
 module.exports = add
