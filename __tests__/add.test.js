ab4b91b32a22602ea77d51fdc9055a3d844d3ade
Wahab896 <memonwahab19@gmail.com>
2024-06-20 15:21:08 +0530

    added formatting and enclosing inputs as string

--- __tests__/add.test.js
+++ __tests__/add.test.js
@@ -1,15 +1,19 @@
 test('Return 0 when nothing is passed', () => {
-    expect(add()).toBe(0);
+    expect(add("")).toBe(0);
 });
 
 test('Return 1 when input is 1', () => {
-    expect(add(1)).toBe(0);
+    expect(add("1")).toBe(0);
 });
 
 test('Return 6 when input is 1,5', () => {
-    expect(add(1,5)).toBe(6);
+    expect(add("1,5")).toBe(6);
 });
 
 test('Return output for multiple inputs', () => {
-    expect(add(1,5,5,10)).toBe(21);
+    expect(add("1,5,5,10")).toBe(21);
 });
+
+test('handle new-line inputs', () => {
+    expect(add("1\n2,3").toBe(6))
+})
