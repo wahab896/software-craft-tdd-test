95d942fff0929df301ad00de42360ebd2b077963
Wahab896 <memonwahab19@gmail.com>
2024-06-20 15:35:04 +0530

    additional expression cases added

--- __tests__/add.test.js
+++ __tests__/add.test.js
@@ -3,24 +3,31 @@ test('Return 0 when nothing is passed', () => {
 });
 
 test('Return 1 when input is 1', () => {
-    expect(add("1")).toBe(0);
+    expect(add("1")).toBe(1);
+    expect(add("4")).toBe(4);
 });
 
 test('Return 6 when input is 1,5', () => {
     expect(add("1,5")).toBe(6);
+    expect(add("9,1")).toBe(10);
 });
 
 test('Return output for multiple inputs', () => {
     expect(add("1,5,5,10")).toBe(21);
+    expect(add("5,5,5,5,5")).toBe(25);
 });
 
 test('Handle new-line inputs', () => {
     expect(add("1\n2,3").toBe(6))
-})
+    expect(add("1\n2,\n3,4").toBe(10));
+});
 
 test('Delimiters as inputs', () => {
     expect(add("//;\n1;2").toBe(3))
-})
+    expect(add("//,1\n1,2").toBe(4))
+    expect(add("1|\n1|2").toBe(4))
+
+});
 
 test('Exception on negative numbers', () => {
     expect(add("1,-1").toBe("negative numbers not allowed -1"));
