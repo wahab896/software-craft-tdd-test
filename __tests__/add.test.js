1c5cf2e643db560ac2aa5b879090c2c7419df10e
Wahab896 <memonwahab19@gmail.com>
2024-06-20 15:15:55 +0530

    two inputs test case

--- __tests__/add.test.js
+++ __tests__/add.test.js
@@ -5,3 +5,7 @@ test('Return 0 when nothing is passed', () => {
 test('Return 1 when input is 1', () => {
     expect(add(1)).toBe(0);
 });
+
+test('Return 6 when input is 1,5', () => {
+    expect(add(1,5)).toBe(6);
+});
