360aff2beec222c76242cb4e5925b1aa18d8df3e
Wahab896 <memonwahab19@gmail.com>
2024-06-20 15:28:07 +0530

    Negative number test case

--- __tests__/add.test.js
+++ __tests__/add.test.js
@@ -14,10 +14,15 @@ test('Return output for multiple inputs', () => {
     expect(add("1,5,5,10")).toBe(21);
 });
 
-test('handle new-line inputs', () => {
+test('Handle new-line inputs', () => {
     expect(add("1\n2,3").toBe(6))
 })
 
-test('delimiters as inputs', () => {
+test('Delimiters as inputs', () => {
     expect(add("//;\n1;2").toBe(3))
+})
+
+test('Exception on negative numbers', () => {
+    expect(add("1,-1").toBe("negative numbers not allowed -1"));
+    expect(add("-4,1,-1").toBe("negative numbers not allowed -4,-1"));
 })
